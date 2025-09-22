ac125b1fbeb6acc78c18cb5569a0348c345b05a0
Tam Thai <hoangtam3062002@gmail.com>
2025-09-22 13:33:10 +0200

    feat: add speed insights

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -37,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -65,11 +67,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="container mx-auto p-4 pt-16">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
