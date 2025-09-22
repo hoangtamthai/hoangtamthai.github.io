83de36d21b3c613e2cd8fec0ae30c4b196af382f
Tam Thai <hoangtam3062002@gmail.com>
2025-09-22 13:04:09 +0200

    fix: analytics

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import type { Route } from "./+types/root";
 import "./app.css";
